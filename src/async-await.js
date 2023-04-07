4bdcb8f1881e06f560465318678f0ddfee07c056
jbmaisner <jean-brice.maisner@armado.fr>
2023-04-07 14:37:12 +0200

    working version

---
+++ src/async-await.js
@@ -0,0 +1,79 @@
+module.exports = {
+  type: "suggestion",
+  meta: {
+    docs: {
+      description: `enforce functions with 'Async' naming convention are awaited`,
+      category: `Possible Errors`,
+      recommended: true,
+      url: "",
+    },
+  },
+
+  create: function (context) {
+    const MISSING_AWAIT = "The call to '{{name}}' is missing an await";
+    const EXTRA_AWAIT = "The call to '{{name}}' has an un-needed await";
+
+    const endsWithAsync = (name) => name.endsWith("Async");
+
+    const getName = (node) => {
+      switch (node.type) {
+        case "CallExpression": {
+          return getName(node.callee);
+        }
+
+        case "MemberExpression":
+          return getName(node.property);
+
+        case "Identifier":
+          return { node, name: node.name };
+      }
+
+      return null;
+    };
+
+    // Has the call been awaited?
+    const isAwaited = (node) => {
+      // Immediate parent is Await so it's async
+      return node.parent.type === "AwaitExpression";
+    };
+
+    const isReturned = (node) => {
+      // Immediate parent is "return" so we are returning
+      return node.parent.type === "ReturnStatement";
+    };
+
+    const CallExpression = (node) => {
+      // Grab the IdentifierNode. If this isn't a pattern
+      // we know about just abort
+      const idNode = getName(node);
+      if (!idNode) {
+        return;
+      }
+
+      // Were async functions called with an await?
+      const calledWithAwait = isAwaited(node);
+      const calledWithReturn = isReturned(node);
+      const nameEndsWithAsync = endsWithAsync(idNode.name);
+
+      if (nameEndsWithAsync && !calledWithAwait && !calledWithReturn) {
+        context.report({
+          node: node,
+          message: MISSING_AWAIT,
+          data: { name: idNode.name },
+        });
+      }
+
+      if (!nameEndsWithAsync && calledWithAwait) {
+        context.report({
+          node: node,
+          message: EXTRA_AWAIT,
+          data: { name: idNode.name },
+        });
+      }
+    };
+
+    return {
+      CallExpression,
+    };
+  },
+};
---
+++ src/async-suffix.js
@@ -0,0 +1,127 @@
+module.exports = {
+  type: "suggestion",
+  meta: {
+    docs: {
+      description: `enforce async function naming ends with 'Async'`,
+      category: `Possible Errors`,
+      recommended: true,
+      url: "",
+    },
+  },
+
+  create: function (context) {
+    const MISSING_ASYNC = `Names should end with 'Async' for async functions. Rename '{{name}}' to '{{name}}Async'`;
+    const EXTRA_ASYNC = `Non async names should not end with 'Async'. Rename '{{name}}Async' to '{{name}}'`;
+
+    const endsWithAsync = (name) => name.endsWith("Async");
+
+    const isReturningPromise = (node) => {
+      return node.value?.body?.body?.[0]?.argument?.callee?.name === "Promise";
+    };
+
+    /**
+     * Check a node is valid
+     * @param  {Object}  node       The root node that is being checked
+     * @param  {Object}  identifier The identifier of the root node
+     * @param  {Boolean} isAsync    Whether the node is marked as async
+     */
+    const check = (node, identifier, isAsync) => {
+      // Unknown case that we don't handle
+      if (!identifier || !identifier.name) {
+        return;
+      }
+
+      const nameEndsWithAsync = endsWithAsync(identifier.name);
+
+      if (isAsync && !nameEndsWithAsync) {
+        context.report({
+          node: identifier,
+          message: MISSING_ASYNC,
+          data: { name: identifier.name },
+          // fix: function(fixer) {
+          //     return fixer.insertTextAfter(identifier, "Async");
+          // },
+        });
+      }
+
+      if (!isAsync && nameEndsWithAsync) {
+        const lastIndex = identifier.name.lastIndexOf("Async");
+        const noneAsyncName = identifier.name.substring(0, lastIndex);
+
+        context.report({
+          node: identifier,
+          message: EXTRA_ASYNC,
+          data: { name: noneAsyncName },
+          // fix: function(fixer) {
+          //     return fixer.replaceText(identifier, noneAsyncName);
+          // },
+        });
+      }
+    };
+
+    const VariableDeclarator = (node) => {
+      const init = node.init;
+      const identifier = node.id;
+
+      if (init && identifier) {
+        check(node, identifier, init.async);
+      }
+    };
+
+    const MethodDefinition = (node) => {
+      const identifier = node.key;
+      const functionExpression = node.value;
+
+      if (identifier && functionExpression) {
+        check(
+          node,
+          identifier,
+          functionExpression.async || isReturningPromise(node)
+        );
+      }
+    };
+
+    const FunctionDeclaration = (node) => {
+      const identifier = node.id;
+
+      if (identifier) {
+        check(node, identifier, node.async || isReturningPromise(node));
+      }
+    };
+
+    const AssignmentExpression = (node) => {
+      const identifier = node.left;
+      const functionExpression = node.right;
+      if (
+        identifier &&
+        functionExpression &&
+        functionExpression.async !== undefined
+      ) {
+        check(node, identifier, functionExpression.async);
+      }
+    };
+
+    const Property = (node) => {
+      // Only care for Methods
+      const identifier = node.key;
+      const functionExpression = node.value;
+
+      // Ignore shorthand node definitions
+      if (node.shorthand) {
+        return;
+      }
+
+      if (identifier && functionExpression) {
+        check(node, identifier, functionExpression.async);
+      }
+    };
+
+    return {
+      VariableDeclarator,
+      FunctionDeclaration,
+      MethodDefinition,
+      Property,
+      AssignmentExpression,
+    };
+  },
+};
